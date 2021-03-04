cb608c6698bad14da71d688feb653387084620ce
Hugo Gruson <hugo.gruson@protonmail.com>
2021-03-04 21:40:28 +0100

    First commit

---
+++ get_biorxiv.R
@@ -0,0 +1,31 @@
+library(httr)
+library(glue)
+
+today <- Sys.Date()
+
+req_today <- glue("https://api.biorxiv.org/details/biorxiv/{today-1}/{today}")
+
+today_res <- content(GET(glue("{req_today}/0")))
+
+today_preprints <- today_res$collection
+
+# Manual pagination since httr doesn't support it yet
+total_nb <- today_res$messages[[1]]$total
+
+if (total_nb > 100) {
+  for (cursor in seq(100, total_nb, by = 100)) {
+    today_res <- content(GET(glue("{req_today}/{cursor}")))
+    
+    today_preprints <- c(today_preprints, today_res$collection)
+  }
+}
+
+preprints <- do.call(rbind.data.frame, today_preprints)
+
+yes_words <- readLines("yes.txt")
+no_words  <- readLines("yes.txt")
+
+library(dplyr)
+relevant_preprints <- preprints %>%
+  filter(grepl(paste0("\\b", yes_words, "\\b", collapse = "|"), title, ignore.case = TRUE)) %>%
+  filter(!grepl(paste0("\\b", no_words, "\\b", collapse = "|"), title, ignore.case = TRUE))
