bc9edf5d6cbebf8353eb4d157e31ff7e5a3da2d5
Hugo Gruson <10783929+Bisaloo@users.noreply.github.com>
2024-08-24 21:35:40 +0200

    Switch to httr2
    Fix #1
    & attempt to fix #2

--- R/get_biorxiv.R
+++ R/get_biorxiv.R
@@ -4,23 +4,32 @@ get_biorxiv <- function() {
   today <- Sys.Date()
 
   req_today <- glue::glue("https://api.biorxiv.org/details/biorxiv/{today-1}/{today}")
+  
+  next_req <- function(resp, req) {
+    msg <- httr2::resp_body_json(resp)$messages[[1]]
+    new_cursor <- as.numeric(msg$cursor) + msg$count
+    total_papers <- as.numeric(msg$total)
+    httr2::signal_total_pages(total_papers)
 
-  today_res <- httr::content(httr::GET(glue::glue("{req_today}/0")))
-
-  today_preprints <- today_res$collection
-
-  # Manual pagination since httr doesn't support it yet
-  total_nb <- today_res$messages[[1]]$total
-
-  if (total_nb > 100) {
-    for (cursor in seq(100, total_nb, by = 100)) {
-      today_res <- httr::content(httr::GET(glue::glue("{req_today}/{cursor}")))
-
-      today_preprints <- c(today_preprints, today_res$collection)
+    if (total_papers <= new_cursor + 1) {
+      return(NULL)
     }
+
+    httr2::request(req_today) |> 
+      httr2::req_url_path_append(new_cursor)
   }
 
-  preprints <- do.call(rbind.data.frame, today_preprints)
+  today_res <- httr2::request(req_today) |> 
+    httr2::req_url_path_append("0") |> 
+    httr2::req_throttle(15 / 60) |>
+    httr2::req_perform_iterative(next_req = next_req) 
+  
+  preprints <- today_res |> 
+    httr2::resps_data(function(resp) {
+      httr2::resp_body_json(resp)$collection
+    }) |> 
+    dplyr::bind_rows()
+
   preprints_v1 <- preprints[preprints$version == 1, ]
 
   yes_words <- readLines(here::here("yes.txt"))
